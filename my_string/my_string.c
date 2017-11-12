867d70be87aaf029f858b3128e2162975101681b
Gil Mansharov <gilmansharov@gmail.com>
2017-11-11 16:07:31 -0800

    Add files via upload

--- my_string/my_string.c
+++ my_string/my_string.c
@@ -1,14 +1,25 @@
+/*
+Author: Gil Mansharov
+ID: 313260192
+
+File Name: my_string.c
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #define CS_IS_BIGGER    1
-#define CT_IS_BIGGER    -1
 #define CS_EQUAL_TO_CT  0
+#define CT_IS_BIGGER    -1
+#define CHAR_IS_NOT_EXIST   -1
 #define MAX_LENGTH  128
 
 
 /*
-
+This function takes 2 strings as input, and returns:
+-1 if cs is before ct in lexicographical order
+0 if cs and ct are equal
+1 if cs is after ct in lexicographical order
 */
 int my_strcmp(const char* cs, const char* ct)
 {
@@ -30,6 +41,12 @@ int my_strcmp(const char* cs, const char* ct)
 
 }
 
+/*
+This function takes 2 strings and an integer as input, and returns:
+-1 if cs is before ct in lexicographical order in the first n characters
+0 if cs and ct are equal the first n characters
+1 if cs is after ct in lexicographical order the first n characters
+*/
 int my_strncmp(const char* cs, const char* ct, int n)
 {
     int min_length, i, cs_length, ct_length, length;
@@ -51,6 +68,11 @@ int my_strncmp(const char* cs, const char* ct, int n)
     return CS_EQUAL_TO_CT;
 }
 
+/*
+This function takes a string and a character as input, and returns the index
+of the first occurrence of c in cs.
+if cs does not contain c, the function returns -1
+*/
 int my_strchr(const char* cs, char c)
 {
     int i, length;
@@ -58,18 +80,36 @@ int my_strchr(const char* cs, char c)
     for (i = 0; i < length; i++)
         if (cs[i] == c)
             return i;
-    return -1;
+    return CHAR_IS_NOT_EXIST;
 }
 
+
+/*
+main function to call for one function with a number from 1-3.
+1 for my_strcmp
+2 for my_strncmp
+3 for my_strchr
+
+and then typing the parameters you want to call the function with.
+
+The main function prints to the standard output the function name that called,
+the parameters, and the result that the function has returned
+*/
 int main()
 {
+    enum functions {
+        STR_CMP = 1,
+        STR_N_CMP = 2,
+        STR_CHR = 3
+    };
+
     int funcNumber;
-    printf("Hello, Please type a number from 1 to 3, in order to call one of the functions:\n");
+    printf("Please type a number from 1 to 3, in order to call one of the functions:\n");
     printf("my_strcmp: type 1\nmy_strncmp: type 2\nmy_strchr: type 3\n");
     scanf("%d", &funcNumber);
     switch(funcNumber)
     {
-        case 1:
+        case STR_CMP:
             {
                 int result;
                 char cs[MAX_LENGTH], ct[MAX_LENGTH];
@@ -81,7 +121,7 @@ int main()
                 printf("The function returned: %d\n", result);
             }
             break;
-        case 2:
+        case STR_N_CMP:
             {
                 int n, result;
                 char cs[MAX_LENGTH], ct[MAX_LENGTH];
@@ -93,7 +133,7 @@ int main()
                 printf("The function returned: %d\n", result);
             }
             break;
-        case 3:
+        case STR_CHR:
             {
                 char c, cs[MAX_LENGTH];
                 int result;
